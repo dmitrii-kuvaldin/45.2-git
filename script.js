bc9addcbc9db4e36444baf64e696384671befe8f
Ernist <ernist.k5@gmail.com>
2024-11-14 22:01:25 +0100

    add count button

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 console.log('hello, Ruslana!')
 console.log(100 + 900)
+
+document.querySelector('#minus').onclick = (event) => {
+        event.preventDefault(); 
+        const inputField = document.querySelector('input[type="number"]');
+        inputField.stepDown(); 
+    };
+
+    document.querySelector('#pow').onclick = (event) => {
+        event.preventDefault();
+        const inputField = document.querySelector('input[type="number"]');
+        inputField.stepUp(); 
+    };
